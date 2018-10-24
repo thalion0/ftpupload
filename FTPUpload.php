d6ffeb816fd5fe44f3c3e28f3b7aceb973285d02
ThoLanger <thomas.langer@keep-it-simple.net>
2018-10-24 20:35:05 +0200

    Initial Commit

---
+++ FTPUpload.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Uploads a folder to a remote folder via FTP protocol recursively
+ * Adapted
+ * User: tholanger
+ * git:
+ * Date: 24.10.2018
+ * Time: 17:25
+ */
+
+class FTPUpload
+{
+
+    private $objConnection;
+
+    public function __construct($strServer, $strUsername, $strPasswort) {
+
+        $this->objConnection = ftp_connect($strServer);
+
+        if(
+            !ftp_login($this->objConnection, $strUsername, $strPasswort)
+        ) {
+            echo "FTP Connection failed! Close Application.";
+            exit;
+        }
+
+        echo "Connected with user " . $strUsername . PHP_EOL;
+        echo "Current directory: " . ftp_pwd($this->objConnection) . PHP_EOL;
+
+
+    }
+
+    public function Upload($strLocalDir, $strRemoteDir) {
+
+        $dirLocal = opendir($strLocalDir);
+
+        while( FALSE !== ($filFile = readdir($dirLocal)) ) {
+            if($filFile != "." && $filFile != "..") {
+                if(is_dir($strLocalDir."/".$filFile)) {
+
+                    if(!ftp_chdir($this->objConnection, $strRemoteDir."/".$filFile)) {
+                        ftp_mkdir($this->objConnection, $strRemoteDir."/".$filFile);
+                    }
+                    $this->Upload($strLocalDir."/".$filFile, $strRemoteDir."/".$filFile);
+                } else {
+                    ftp_put($this->objConnection, $strRemoteDir . "/" . $filFile, $strLocalDir . "/" . $filFile, FTP_BINARY);
+                }
+            }
+        }
+
+    }
+
+}
+
+// Create an instance of Class FTPUpload
+// As parameters use the credentials
+$objUpload = new FTPUpload("SERVER", "USERNAME", "PASSWORD");
+
+// Uploads the Files, please change LocalDir and Remotedir to your needs
+$objUpload->Upload("./Test", "/Test");
